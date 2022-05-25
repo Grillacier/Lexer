5fc8c83bac0aa12461895a9d3f131e2f545435b5
cezhou52 <zhoucedric02000@gmail.com>
2022-05-26 00:55:55 +0200

    main typo pour un palindrome qui marche

--- main.ml
+++ main.ml
@@ -3,5 +3,5 @@ let lexbuf = Lexing.from_channel stdin
 let ast = Parser.input Lexer.main lexbuf
 
 let transi = Ast.recupTransitions ast
-let config = Ast.init "abcbca" ast
+let config = Ast.init "abcba" ast
 let _ = Ast.lancer config transi

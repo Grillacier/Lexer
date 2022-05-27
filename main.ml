20f5f5ba862af0ab763f32964692a82e04a8f010
Zhou Cédric <zhoucedric02000@gmail.com>
2022-05-27 16:36:58 +0200

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,7 +1,10 @@
 let lexbuf = Lexing.from_channel stdin
-let ast =Parser.input Lexer.main lexbuf (*cree automate*)
-
-let transi = Ast.recupTransitions ast (*recupere les transitions*)
-let () = print_string (Ast.stringTransitionsList (Ast.recupTransitions ast ))
+let ast =
+try
+	Parser.input Lexer.main lexbuf (*cree automate*)
+with
+	Parser.Error -> failwith "erreur syntaxique"
+let transi = Ast.recupTransitions ast (*cree liste de transitions*)
+let () = print_string (Ast.stringTransitionsList (Ast.recupTransitions ast )) (* affiche la liste des transitions, surtout pour la partie 3, pour vérifier les transitions *)
 let config = Ast.init Sys.argv.(1) ast (*initialise config avec arg du terminal*)
-let _ = Ast.lancer config transi
+let _ = Ast.lancer config transi (* executer l'automate sur le mot *)
